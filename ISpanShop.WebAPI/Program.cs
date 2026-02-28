97af5e56fa7ca3b052d1180512f3a0647177ae4c
Berlin <a0965828360@gmail.com>
2026-02-28 11:28:50 +0800

    建立連線

--- ISpanShop.WebAPI/Program.cs
+++ ISpanShop.WebAPI/Program.cs
@@ -1,4 +1,7 @@
 
+using ISpanShop.Models.EfModels;
+using Microsoft.EntityFrameworkCore;
+
 namespace ISpanShop.WebAPI
 {
 	public class Program
@@ -6,6 +9,9 @@ namespace ISpanShop.WebAPI
 		public static void Main(string[] args)
 		{
 			var builder = WebApplication.CreateBuilder(args);
+
+			builder.Services.AddDbContext<ISpanShopDBContext>(options =>
+			options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 			// Add services to the container.
 
