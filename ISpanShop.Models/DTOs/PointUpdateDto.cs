f4f9ed8f4d719075e57b053f0bdcaa870c2c6d3e
wei <xa992000@gmail.com>
2026-03-04 10:44:18 +0800

    Gemini 說了 太棒了！那我們就從最核心的 PointService 開始。
    點數系統是金流中「邏輯最嚴謹」的部分，因為它直接涉及使用者的資產。我們會先在 ISpanShop.Services 專案中建立這個服務。
    
    1. 定義 DTO (在 ISpanShop.Models)
    在處理邏輯前，我們需要一個乾淨的資料傳輸物件來記錄異動。

---
+++ ISpanShop.Models/DTOs/PointUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISpanShop.Models.DTOs
+{
+	public class PointUpdateDTO
+	{
+		public int UserId { get; set; }
+		public int ChangeAmount { get; set; } // 正值為增加，負值為折抵
+		public string OrderNumber { get; set; }
+		public string Description { get; set; }
+	}
+}
